bf59a58ee80c85da3fb226938ce7b3e4cfd49a91
627876093 <627876093@qq.com>
2017-08-22 09:37:26 +0800

    填写描述（必须）

--- app/src/main/java/cn/com/zlct/diamondgo/activity/textgithub.java
+++ app/src/main/java/cn/com/zlct/diamondgo/activity/textgithub.java
@@ -4,4 +4,5 @@ package cn.com.zlct.diamondgo.activity;
  * Created by Administrator on 2017/8/21 0021.
  */
 public class textgithub {
+    String str;
 }
--- app/src/main/java/cn/com/zlct/diamondgo/activity/textgithubs.java
+++ app/src/main/java/cn/com/zlct/diamondgo/activity/textgithubs.java
@@ -4,4 +4,6 @@ package cn.com.zlct.diamondgo.activity;
  * Created by Administrator on 2017/8/21 0021.
  */
 public class textgithubs {
+
+    //更新
 }
